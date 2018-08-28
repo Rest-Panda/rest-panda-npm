999ce8f53b0279aa40d115ea0fd2f48b09d895f2
mfilho <moises.filho.ext@coriant.com>
2018-08-28 10:55:59 +0100

    Created initial project configuration

---
+++ rest-panda.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const {prompt} = require('inquirer');
+const cli = require('commander');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter username account:'
+    },
+    {
+        type: 'password',
+        name: 'password',
+        message: 'Enter password account:'
+    }
+]
+
+cli
+    .version('0.0.1')
+    .usage('[command] <params>')
+    .description('CLI to manager the Rest Panda project');
+
+cli
+    .command('app <app>')
+    .action(function (app, cmd) {
+        console.log('*** Please inform your credentials to access the Rest Panda account ***');
+        prompt(questions)
+            .then(answers => console.log(answers))
+    })
+
+cli
+    .parse(process.argv);
